ea0383b8c2bd84faa963a217cb73179215af73ec
Paulo Pimentel <fudevil123@gmail.com>
2024-12-16 17:12:01 -0300

    fixing eslint, motion, type errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    "rules": {
+      "react/no-unescaped-entities": 0,
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error"
+      ]
+    }
+  }
 ];
 
 export default eslintConfig;
